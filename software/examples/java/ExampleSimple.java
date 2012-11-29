85d8201b246e78e10e8a9d758aa873e068c08559
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 14:43:09 +0100

    Make Java examples Protocol 2.0 compatible

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -7,15 +7,13 @@ public class ExampleSimple {
 	private static final String UID = "ABC"; // Change to your UID
 	
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
-	//       might normally want to catch are described in the commnents below
+	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
-		// Create connection to brickd
-		IPConnection ipcon = new IPConnection(host, port); // Can throw IOException
-		BrickletTemperatureIR tir = new BrickletTemperatureIR(UID); // Create device object
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletTemperatureIR tir = new BrickletTemperatureIR(UID, ipcon); // Create device object
 
-		// Add device to IP connection
-		ipcon.addDevice(tir); // Can throw IPConnection.TimeoutException
-		// Don't use device before it is added to a connection
+		ipcon.connect(host, port); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Get current object and ambient temperature (unit is °C/10)
 		short obj = tir.getObjectTemperature(); // Can throw IPConnection.TimeoutException
@@ -25,6 +23,5 @@ public class ExampleSimple {
 		System.out.println("Ambient Temperature: " + amb/10.0 + " °C");
 
 		System.console().readLine("Press key to exit\n");
-		ipcon.destroy();
 	}
 }
--- software/examples/java/ExampleWaterBoiling.java
+++ software/examples/java/ExampleWaterBoiling.java
@@ -7,15 +7,13 @@ public class ExampleWaterBoiling {
 	private static final String UID = "ABC"; // Change to your UID
 	
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
-	//       might normally want to catch are described in the commnents below
+	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
-		// Create connection to brickd
-		IPConnection ipcon = new IPConnection(host, port); // Can throw IOException
-		BrickletTemperatureIR tir = new BrickletTemperatureIR(UID); // Create device object
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletTemperatureIR tir = new BrickletTemperatureIR(UID, ipcon); // Create device object
 
-		// Add device to IP connection
-		ipcon.addDevice(tir); // Can throw IPConnection.TimeoutException
-		// Don't use device before it is added to a connection
+		ipcon.connect(host, port); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Set emissivity to 0.98 (emissivity of water)
 		tir.setEmissivity((int)(0xFFFF*0.98));
@@ -37,6 +35,5 @@ public class ExampleWaterBoiling {
 		});
 
 		System.console().readLine("Press key to exit\n");
-		ipcon.destroy();
 	}
 }
