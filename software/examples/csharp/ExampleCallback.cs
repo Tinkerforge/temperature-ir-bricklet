11baa9570909cf4883544e59e29db215cd658399
Olaf Lüke <olaf@tinkerforge.com>
2012-12-05 16:01:47 +0100

    Add protocol 2.0 compatability to C# bindings

--- software/examples/csharp/ExampleCallback.cs
+++ software/examples/csharp/ExampleCallback.cs
@@ -8,22 +8,23 @@ class Example
 
 	// Callback functions for object/ambient temperature callback 
 	// (parameters have unit °C/10)
-	static void ObjectCB(short temperature)
+	static void ObjectCB(object sender, short temperature)
 	{
 		System.Console.WriteLine("Object Temperature: " + temperature/10.0 + " °C");
 	}
 
-	static void AmbientCB(short temperature)
+	static void AmbientCB(object sender, short temperature)
 	{
 		System.Console.WriteLine("Ambient Temperature: " + temperature/10.0 + " °C");
 	}
 
 	static void Main() 
 	{
-		IPConnection ipcon = new IPConnection(HOST, PORT); // Create connection to brickd
-		BrickletTemperatureIR temp = new BrickletTemperatureIR(UID); // Create device object
-		ipcon.AddDevice(temp); // Add device to IP connection
-		// Don't use device before it is added to a connection
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletTemperatureIR temp = new BrickletTemperatureIR(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Set Period for temperature callbacks to 1s (1000ms)
 		// Note: The callbacks are only called every second if the 
@@ -32,12 +33,11 @@ class Example
 		temp.SetAmbientTemperatureCallbackPeriod(1000);
 
 		// Register object temperature callback to function ObjectCB
-		temp.RegisterCallback(new BrickletTemperatureIR.ObjectTemperature(ObjectCB));
+		temp.ObjectTemperature += ObjectCB;
 		// Register ambient temperature callback to function AmbientCB
-		temp.RegisterCallback(new BrickletTemperatureIR.AmbientTemperature(AmbientCB));
+		temp.AmbientTemperature += AmbientCB;
 
 		System.Console.WriteLine("Press key to exit");
 		System.Console.ReadKey();
-		ipcon.Destroy();
 	}
 }
--- software/examples/csharp/ExampleWaterBoiling.cs
+++ software/examples/csharp/ExampleWaterBoiling.cs
@@ -7,7 +7,7 @@ class Example
 	private static string UID = "ABC"; // Change to your UID
 
 	// Callback for object temperature greater than 100 °C (parameter has unit °C/10)
-	static void ReachedCB(short temperature)
+	static void ReachedCB(object sender, short temperature)
 	{
 		System.Console.WriteLine("The surface has a temperature of " + 
 		                         temperature/10.0 + 
@@ -17,10 +17,11 @@ class Example
 
 	static void Main() 
 	{
-		IPConnection ipcon = new IPConnection(HOST, PORT); // Create connection to brickd
-		BrickletTemperatureIR temp = new BrickletTemperatureIR(UID); // Create device object
-		ipcon.AddDevice(temp); // Add device to IP connection
-		// Don't use device before it is added to a connection
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletTemperatureIR temp = new BrickletTemperatureIR(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Set emissivity to 0.98 (emissivity of water)
 		temp.SetEmissivity((ushort)(0xFFFF*0.98));
@@ -29,13 +30,12 @@ class Example
 		temp.SetDebouncePeriod(1000);
 
 		// Register threshold reached callback to function ReachedCB
-		temp.RegisterCallback(new BrickletTemperatureIR.ObjectTemperatureReached(ReachedCB));
+		temp.ObjectTemperatureReached += ReachedCB;
 
 		// Configure threshold for "greater than 100 °C" (unit is °C/10)
 		temp.SetObjectTemperatureCallbackThreshold('>', 100*10, 0);
 
 		System.Console.WriteLine("Press key to exit");
 		System.Console.ReadKey();
-		ipcon.Destroy();
 	}
 }
